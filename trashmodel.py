6a19fa3d92a61b54f3c8176dbbb66404f588d205
db030220 <72208145+db030220@users.noreply.github.com>
2023-03-28 23:03:48 +0900

    Add files via upload

---
+++ trashmodel.py
@@ -0,0 +1,8 @@
+import cv2
+import numpy as np
+
+net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+classes = []
+with open("coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+layer_names = net.getLayerNames()
