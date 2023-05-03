1cb8206ae1659f1495c89ee7fb8793329403d824
Park-jungyeon <75595021+Park-jungyeon@users.noreply.github.com>
2023-05-03 17:23:37 +0900

    Update HomaMain.js

--- src/screens/HomaMain.js
+++ src/screens/HomaMain.js
@@ -1,13 +1,44 @@
-import {WebView} from "react-native-webview";
+import React from 'react'
+import {WebView} from 'react-native-webview';
+import { View } from 'react-native';
+import watch from '../addons/watch'
+import Pedometer from '../addons/Pedometer'
 
-export default class Location extends Component{
 
+const KakaoMapScreen = () =>{
+    const apiKey = '093f44ff0baa195ab8c672ddce75f0fd';
+    const url = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${apiKey}`;
+    const html = `
+        <html>
+        <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <script type="text/javascript" src="${url}"></script>
+        </head>
+        <body>
+            <div id="map" style="width:20==100%;height:70%;"></div>
+            <script>
+            const container = document.getElementById('map');
+            const options = {
+                center: new kakao.maps.LatLng(37.566826, 126.9786567),
+                level: 3,
+            };
+            const map = new kakao.maps.Map(container, options);
+            </script>
+        </body>
+        </html>
+        `;
 
-  render(){
     return(
-      <WebView
-      source={{uri: "http://localhost:8080/"}}
-      />
-    );
-  }
+        <View style={{flex:1}}>
+            <WebView
+            originWhitelist={['*']}
+            source={{ html }}
+            javaScriptEnabled={true}
+            injectedJavaScript={''}
+            style={{ flex: 1 }}
+            />
+        </View>
+    )
 }
+
+export default KakaoMapScreen;
