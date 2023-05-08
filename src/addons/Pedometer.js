86f831c327083adb0678e95373be7e13c170a239
Park-jungyeon <75595021+Park-jungyeon@users.noreply.github.com>
2023-05-08 13:26:28 +0900

    Update Pedometer.js

--- src/addons/Pedometer.js
+++ src/addons/Pedometer.js
@@ -1,74 +1,41 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Text, View } from 'react-native';
 import { Accelerometer } from 'expo-sensors';
 
-export default function App() {
-  const [isRunning, setIsRunning] = useState(false);
+export default function StepCounter() {
   const [steps, setSteps] = useState(0);
-  const [lastStepTime, setLastStepTime] = useState(null);
-
+  
   useEffect(() => {
     let subscription;
-    if (isRunning) {
-      subscription = Accelerometer.addListener(handleAccelerometerData);
-    } else {
-      subscription?.remove();
-    }
-    return () => subscription?.remove();
-  }, [isRunning]);
-
-  const handleAccelerometerData = ({ x, y, z }) => {
-    const acceleration = Math.sqrt(x * x + y * y + z * z) - 1;
-    if (acceleration > 1) {
-      const now = Date.now();
-      if (!lastStepTime || now - lastStepTime > 300) {
-        setSteps(steps + 1);
-        setLastStepTime(now);
+    Accelerometer.setUpdateInterval(1000); // 1초마다 업데이트
+  
+    const calculateSteps = ({ x, y, z }) => {
+      const acceleration = Math.sqrt(x*x + y*y + z*z);
+      if (acceleration > 1.2) { // 걸음걸이 감지
+        setSteps(prevState => prevState + 1);
       }
-    }
-  };
-
-  const handleStartStopPress = () => {
-    setIsRunning(!isRunning);
-    setSteps(0);
-    setLastStepTime(null);
-  };
-
+    };
+  
+    Accelerometer.isAvailableAsync().then(
+      available => {
+        if (available) {
+          subscription = Accelerometer.addListener(calculateSteps);
+        } else {
+          console.log("Accelerometer is not available on this device.");
+        }
+      }
+    );
+  
+    return () => {
+      if (subscription) {
+        subscription.remove();
+      }
+    };
+  }, []);
+  
   return (
-    <View style={styles.container}>
-      <Text style={styles.stepsText}>{steps} steps</Text>
-      <TouchableOpacity
-        style={styles.startStopButton}
-        onPress={handleStartStopPress}
-      >
-        <Text style={styles.startStopButtonText}>
-          {isRunning ? 'Stop' : 'Start'}
-        </Text>
-      </TouchableOpacity>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 24 }}>걸음 수: {steps}</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  stepsText: {
-    fontSize: 48,
-    fontWeight: 'bold',
-    marginBottom: 32,
-  },
-  startStopButton: {
-    backgroundColor: '#2196F3',
-    paddingVertical: 16,
-    paddingHorizontal: 32,
-    borderRadius: 8,
-  },
-  startStopButtonText: {
-    color: '#FFF',
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-});
