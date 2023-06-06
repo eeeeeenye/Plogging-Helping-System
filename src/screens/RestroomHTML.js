0d5bfe43433edda3c7858c756bcdb71be8221714
Park-jungyeon <75595021+Park-jungyeon@users.noreply.github.com>
2023-06-06 10:59:59 +0900

    Create RestroomHTML.js

---
+++ src/screens/RestroomHTML.js
@@ -0,0 +1,43 @@
+const RestroomSet =(url,position)=>{ 
+  return(`
+    <html>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=093f44ff0baa195ab8c672ddce75f0fd&libraries=services"></script>
+        <style>
+          #map {
+            width: 100%;
+            height: 100%;
+          }
+        </style>
+      </head>
+      <body>
+        <div id="map"></div>
+        <script>
+          const markers = ${JSON.stringify(markers)}; // 가져온 화장실 위치 데이터
+
+          // Kakao 지도 API 초기화 
+          kakao.maps.load(() => {
+            const container = document.getElementById('map');
+            const options = {
+              center: new kakao.maps.LatLng(37.5665, 126.9780), // 초기 지도 중심 좌표 설정
+              level: 3, // 초기 지도 확대 레벨 설정
+            };
+            const map = new kakao.maps.Map(container, options);
+
+            // 화장실 위치에 마커 생성
+            markers.forEach((marker) => {
+              const markerPosition = new kakao.maps.LatLng(marker.latitude, marker.longitude);
+              const marker = new kakao.maps.Marker({
+                position: markerPosition,
+              }); 
+              marker.setMap(map);
+            });
+          });
+        </script>
+      </body>
+    </html>
+  `)
+}
+
+export default RestroomSet;
