948e5d873b9591363934d60a343a23e5efb77092
in hye yoo <75657062+eeeeeenye@users.noreply.github.com>
2023-06-06 15:06:12 +0900

    Create Public.js

---
+++ src/screens/Public.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
+import Constants from 'expo-constants';
+import RestroomSet from '../map/htmlCode/RestroomHTML';
+import axios from 'axios';
+
+
+const Public_toilet = () =>{
+  const [toiletData, setToiletData] = useState([]);
+  const ip = Constants.manifest.extra.Local_ip;
+
+  useEffect(() => {
+  fetchData();
+  }, []);
+
+  useEffect(()=>{
+    console.log(toiletData)
+  },[toiletData])
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`http://${ip}:3000/publicToilets`);
+      
+      console.log(toiletData)
+
+      // 가져온 데이터 정제하기, 도로명, 지번, 위도, 경도 가져오기
+      const refinedData = response.data.map((item) => {
+        return {
+          rdnmadr: item.rdnmadr,
+          lnmadr: item.lnmadr,
+          latitude: item.latitude,
+          longitude: item.longitude,
+        };
+      });
+
+      setToiletData(refinedData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <WebView source={RestroomSet} style={styles.map} />
+      {/* <MyComponent toiletData={toiletData} /> */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  map: {
+    flex: 1,
+  },
+});
+
+export default Public_toilet
