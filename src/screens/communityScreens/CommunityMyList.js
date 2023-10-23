90721d2fc60bd2d193e065830f8975e490dfd3d3
dpemdnjem23 <dpemdnjem23@gmail.com>
2023-10-23 23:31:23 +0900

    참여 모임목록

--- src/screens/communityScreens/CommunityMyList.js
+++ src/screens/communityScreens/CommunityMyList.js
@@ -25,13 +25,71 @@ const CommunityMyList = () => {
   return (
     <View style={styles.container}>
       <HeaderScroll3 title={'탕정면'}>
-        <View style={styles.content}>
+        <View style={styles.contents}>
           <View style={styles.row}>
-            <View>
+            <View style={styles.image_box}>
               <Image
                 style={styles.image}
                 source={require('../../assets/Rectangle40.png')}
               ></Image>
+            </View>
+            <View style={styles.text_box}>
+              <Text style={[styles.title, styles.line]}>
+                온양온천역 근처에서 플로깅
+              </Text>
+              <Text style={[styles.tag, styles.line]}>
+                #아산 #사교 #취미공유
+              </Text>
+
+              <Text style={[styles.members, styles.line]}>
+                아산시 | 참여 멤버 3
+              </Text>
+
+              <Text style={[styles.date, styles.line]}>2023.03.10</Text>
+            </View>
+          </View>
+          <View style={styles.row}>
+            <View style={styles.image_box}>
+              <Image
+                style={styles.image}
+                source={require('../../assets/Rectangle40.png')}
+              ></Image>
+            </View>
+            <View style={styles.text_box}>
+              <Text style={[styles.title, styles.line]}>
+                온양온천역 근처에서 플로깅
+              </Text>
+              <Text style={[styles.tag, styles.line]}>
+                #아산 #사교 #취미공유
+              </Text>
+
+              <Text style={[styles.members, styles.line]}>
+                아산시 | 참여 멤버 3
+              </Text>
+
+              <Text style={[styles.date, styles.line]}>2023.03.10</Text>
+            </View>
+          </View>
+          <View style={styles.row}>
+            <View style={styles.image_box}>
+              <Image
+                style={styles.image}
+                source={require('../../assets/Rectangle40.png')}
+              ></Image>
+            </View>
+            <View style={styles.text_box}>
+              <Text style={[styles.title, styles.line]}>
+                온양온천역 근처에서 플로깅
+              </Text>
+              <Text style={[styles.tag, styles.line]}>
+                #아산 #사교 #취미공유
+              </Text>
+
+              <Text style={[styles.members, styles.line]}>
+                아산시 | 참여 멤버 3
+              </Text>
+
+              <Text style={[styles.date, styles.line]}>2023.03.10</Text>
             </View>
           </View>
         </View>
--- src/screens/communityScreens/communityStyles/CommunityMyListStyle.js
+++ src/screens/communityScreens/communityStyles/CommunityMyListStyle.js
@@ -17,83 +17,28 @@ const styles = StyleSheet.create({
     marginBottom: 50,
   },
   image_box: {
-    marginRight: 10,
+    marginRight: 15,
   },
   image: {
-    width: 36,
-    height: 30,
-    borderRadius: 50,
+    width: 84,
+    height: 80,
+    // borderRadius: 50,
     flexDirection: 'row',
   },
   row: {
+    width: width * 0.8,
     marginTop: 28,
     flexDirection: 'row',
   },
-  text_box: {},
-  text1: {
-    fontWeight: 700,
-    lineHeight: 28,
-    fontSize: 16,
+  line: { lineHeight: 20 },
+
+  title: { lineHeight: 20, fontWeight: 700, fontSize: 15 },
+  tag: {
+    fontSize: 13,
   },
-  text2: {
+  members: { fontSize: 14 },
+  date: {
     fontSize: 12,
-    fontWeight: 400,
-  },
-  ranking_background: {
-    position: 'absolute',
-    zIndex: 5,
-    bottom: 30,
-    right: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 50,
-    width: 35,
-    height: 35,
-    // borderWidth: 1,
-  },
-  ranking_round: {
-    borderWidth: 1,
-    width: 25,
-    height: 25,
-    justifyContent: 'center',
-    alignItems: 'center',
-
-    borderRadius: 50,
-  },
-  first: { backgroundColor: 'gold' },
-  second: {
-    backgroundColor: 'silver',
-  },
-  third: {
-    backgroundColor: 'bronze',
-  },
-  ss: {
-    borderWidth: 1,
-  },
-  ranking_background2: {
-    position: 'absolute',
-    zIndex: 5,
-    bottom: 30,
-    right: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 50,
-    width: 35,
-    height: 35,
-    borderWidth: 1,
-    backgroundColor: 'white',
-  },
-  ranking_round2: {
-    width: 25,
-    height: 25,
-    justifyContent: 'center',
-    alignItems: 'center',
-
-    borderRadius: 50,
-  },
-  ranking2: {
-    fontWeight: 700,
-    fontSize: 15,
   },
 })
 
