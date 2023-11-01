17f0d5e4c3e02d3a55ec2e7c7c7099482e55aa4e
gitkrak <dpemdnjem23@gmail.com>
2023-11-01 23:58:49 +0900

    webView

--- src/screens/authScreens/ResidenceSettingScreen.js
+++ src/screens/authScreens/ResidenceSettingScreen.js
@@ -5,15 +5,25 @@ import Logo from '../../components/Logo'
 import Header from '../../components/Header'
 import TextInput from '../../components/TextInput'
 import Button from '../../components/Button'
+
+import { WebView } from 'react-native-webview';
+
 import { emailValidator } from '../../helpers/emailValidator'
-import Postcode from 'react-daum-postcode'
 export default function ResidenceSettingScreen({ navigation }) {
   // Geolocation.requestAuthorization()
   // DaumPostcodeEmbed
+  const handleComplete = (data) => {
+    // 선택된 주소 정보는 'data' 객체에 담겨 옵니다.
+    console.log(data)
+  }
+
   return (
     <Background>
       <Logo />
-      <Postcode></Postcode>
+      <WebView>
+
+        
+      </WebView>
     </Background>
   )
 }
