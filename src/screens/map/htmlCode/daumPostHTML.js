629cfc032aabd2b097772f6066ec63bb5c383cbd
dpemdnjem23 <dpemdnjem23@gmail.com>
2023-11-12 23:44:18 +0900

    webView 통신

--- src/screens/map/htmlCode/daumPostHTML.js
+++ src/screens/map/htmlCode/daumPostHTML.js
@@ -4,15 +4,17 @@ const daumPostSet = (url, position) => {
             <head>
               <meta name="viewport" content="width=device-width, initial-scale=1.0">
               <script type="text/javascript" src="${url}"></script>
+              
             </head>
             <body>
 
               <div id="map" style="width:100%;height:100%;"></div>
-         
-    </div>
-              <script>
+  
+    <h1>Hello, World!</h1>
+    <p id="result"></p>
+    
+              <script  type="text/javascript">
 
-              var geocoder = new kakao.maps.services.Geocoder();
                 const container = document.getElementById('map');
                 const options = {
                   center: new kakao.maps.LatLng(${position?.lat}, ${position?.lng}), // 현재 위치를 기준으로 지도를 보여준다.
@@ -21,6 +23,7 @@ const daumPostSet = (url, position) => {
              
           
                 const map = new kakao.maps.Map(container, options);
+              var geocoder = new kakao.maps.services.Geocoder();
                 
                 const markerPosition = new kakao.maps.LatLng(${position?.lat}, ${position?.lng});
                 const marker = new kakao.maps.Marker({
@@ -30,17 +33,30 @@ const daumPostSet = (url, position) => {
 
     kakao.maps.event.addListener(map, 'center_changed', function() {
         var newCenter = map.getCenter(); // 지도의 새 중심 좌표 가져오기
-        marker.setPosition(newCenter); // 마커의 위치를 새 중심으로 설정
-     
+     marker.setPosition(newCenter); // 마커의 위치를 새 중심으로 설정
+
   
     });  
-
     function searchAddrFromCoords(coords, callback) {
       // 좌표로 행정동 주소 정보를 요청합니다
       geocoder.coord2RegionCode(coords.getLng(), coords.getLat(), callback);         
   }
- 
+  
+  kakao.maps.event.addListener(map, 'dragend', function() {        
+    
+    // 지도 중심좌표를 얻어옵니다 
+    var latlng = map.getCenter(); 
+    
+    var message = '변경된 지도 중심좌표는 ' + latlng.getLat() + ' 이고, ';
+    message += '경도는 ' + latlng.getLng() + ' 입니다';
+    
+    window.ReactNativeWebView.postMessage(JSON.stringify({ lng:  latlng.getLng(),lat:latlng.getLat() }));
+    
+});
+  console.log('console')
 </script>
+
+
     
             </body>
           </html>
--- src/screens/map/myLocationMap.js
+++ src/screens/map/myLocationMap.js
@@ -19,16 +19,17 @@ const MylocationMap = ({ navigation }) => {
   const dispatch = useDispatch()
   const user = useSelector((state) => state.auth.user)
   const name = user ? user.name : ''
-
-  console.log(name, '--------LocationSetting')
+  let latitude
+  let longitude
+  // console.log(name, '--------LocationSetting')
   const getLocation = async () => {
     try {
       await Location.requestForegroundPermissionsAsync()
 
       //현재 위치 정보 얻기 -> 시스템 location
       const locationData = await Location.getCurrentPositionAsync()
-      const latitude = locationData['coords']['latitude'] // 위도 가져오기
-      const longitude = locationData['coords']['longitude'] // 경도 가져오기
+      latitude = locationData['coords']['latitude'] // 위도 가져오기
+      longitude = locationData['coords']['longitude'] // 경도 가져오기
       const location = await Location.reverseGeocodeAsync(
         // 위도와 경도를 입력하면 자신의 위치를 역으로 변환
         { latitude, longitude },
@@ -38,29 +39,25 @@ const MylocationMap = ({ navigation }) => {
       setPosition({ lat: latitude, lng: longitude })
       setCity(location[0])
 
-      axios
-        .get(
-          ` https://dapi.kakao.com/v2/local/geo/coord2address.json?x=${longitude}&y=${latitude}&input_coord=WGS84`,
-          {
-            headers: {
-              Authorization: `KakaoAK ${process.env.REST_API_KEY}`,
-            },
-          }
-        )
-        .then((response) => {
-          const data = response.data
-          if (data.documents && data.documents.length > 0) {
-            const firstDocument = data.documents[0]
-            console.log(firstDocument, 'firstDocu')
-            //   setAddress(firstDocument.address_name)
-          } else {
-            //   setAddress('주소를 찾을 수 없습니다.')
-          }
-        })
-        .catch((error) => {
-          console.error('API 호출 오류:', error)
-        })
-      console.log(position, city)
+      const myLoc = await axios.get(
+        ` https://dapi.kakao.com/v2/local/geo/coord2address.json?x=${longitude}&y=${latitude}&input_coord=WGS84`,
+        {
+          headers: {
+            Authorization: `KakaoAK ${process.env.REST_API_KEY}`,
+          },
+        }
+      )
+
+      const data = myLoc.data
+      if (data.documents && data.documents.length > 0) {
+        const firstDocument = data.documents[0]
+        console.log(firstDocument.address.address_name, 'firstDocu')
+        // setPosition(firstDocument.address.address_name)
+      } else {
+        //   setAddress('주소를 찾을 수 없습니다.')
+      }
+
+      // console.log(position, city)
 
       //   const response = await axios.get(
       //     `https://dapi.kakao.com/v2/local/geo/coord2address.json?x=${longitude}&y=${latitude}`,
@@ -85,20 +82,20 @@ const MylocationMap = ({ navigation }) => {
       Alert.alert(error)
     }
   }
-  // const onPressButton = async () => {
-  //   // 버튼을 누르면 작동하는 기능들 (회원 상태값 업데이트, 화면전환)
-  //   try {
-  //     console.log(city, name, '----------------->>>>')
-  //     await axios.put(`http://${ip}:3000/plogging/:params`, {
-  //       ClientName: name,
-  //       city: city,
-  //     })
-  //     dispatch(addAdress({ adress: city, status: true }))
-  //     navigation.navigate('HomeMain')
-  //   } catch (event) {
-  //     console.log(event)
-  //   }
-  // }
+  const onPressButton = async () => {
+    // 버튼을 누르면 작동하는 기능들 (회원 상태값 업데이트, 화면전환)
+    try {
+      console.log(city, name, '----------------->>>>')
+      await axios.put(`http://${ip}:3000/plogging/:params`, {
+        ClientName: name,
+        city: city,
+      })
+      dispatch(addAdress({ adress: city, status: true }))
+      navigation.navigate('HomeMain')
+    } catch (event) {
+      console.log(event)
+    }
+  }
 
   useEffect(() => {
     const fetchData = async () => {
@@ -106,7 +103,7 @@ const MylocationMap = ({ navigation }) => {
     }
 
     fetchData()
-  }, [])
+  }, [latitude, longitude])
   // position이 존재하지 않으면 렌더링하지 않는다.
   if (!position) {
     return null
@@ -115,7 +112,6 @@ const MylocationMap = ({ navigation }) => {
   // 카카오 맵 API를 사용하기 위한 설정값
   const apiKey = Constants.manifest.extra.KAKAO_JAVASCRIPT_KEY
   const url = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${apiKey}&libraries=services`
-
   return (
     <View style={{ flex: 1 }}>
       <WebView
@@ -123,13 +119,19 @@ const MylocationMap = ({ navigation }) => {
         source={{ html: daumPostSet(url, position) }}
         javaScriptEnabled={true}
         injectedJavaScript={''}
+        domStorageEnabled={true}
         style={{
           flex: 1,
           // top: ,
         }}
+        onMessage={(event) => {
+          console.log(event.nativeEvent.data)
+          latitude = event.nativeEvent.data.lat
+          longitude = event.nativeEvent.data.lng
+        }}
       />
       <View style={{ flex: 0.4 }}>
-        <Text>대전 유성구 은구비서로 </Text>
+        {/* <Text>{position}</Text> */}
         <Button mode="outlined">주소 설정</Button>
       </View>
     </View>
